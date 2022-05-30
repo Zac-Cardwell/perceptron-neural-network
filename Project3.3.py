1a52e1e56012f3c426c46f33b38ab3ce3ef93892
Zac Cardwell <59982490+Zac-Cardwell@users.noreply.github.com>
2022-05-29 22:59:40 -0400

    Add files via upload

--- Project3.3.py
+++ Project3.3.py
@@ -1,25 +1,36 @@
 import numpy as np
-from AI_project3_1 import *
+import pandas as pd
+from project3_1 import *
 
-# determines if input is a mammal inputs represent breaths air, has feathers, can fly
-# as long as breathes air is true and has feathers is false it should return a true statment
-train_data = np.array([[0,0,0],[0,0,1],[0,1,0],[1,0,0]])
-train_answer = np.array([[0],[0],[0],[1]])
-test_data = np.array([[1,0,1],[1,1,0],[1,1,1],[0,1,1]])
-test_answer = np.array([[1],[0],[0],[0]])
+df = pd.read_csv('heart_disease.csv')
+df = df.drop(columns = ['BMI', 'GenHlth', 'MentHlth', 'PhysHlth', 'Age', 'Education', 'Income'], axis = 1)
+df.loc[df["Diabetes"] == 2, "Diabetes"] = 1
 
-test = percep(train_data.shape[1], learning_rate=0.75)
+train_data = np.array(df.iloc[0:100, 1:15].values.tolist())
+train_answer = np.array(df.iloc[0:100, 0:1].values.tolist())
+test_data = np.array(df.iloc[100:200, 1:15].values.tolist())
+test_answer = np.array(df.iloc[100:200, 0:1].values.tolist())
+
+test = percep(train_data.shape[1], learning_rate=0.0025)
 print(test.weight)
-test.fit(train_data, train_answer, epochs=200)
+test.fit(train_data, train_answer, epochs=50000)
 print(test.weight)
 
+wronga = 0
 print("Training data final answers")
 for (x, target) in zip(train_data, train_answer):
     answer = test.prediction(x)
+    if answer != target[0]:
+        wronga += 1
     print("data={}, answer={}, pred={}".format(x, target[0], answer))
 
-
+wrong = 0
 print("\nTest data answers")
 for (x, target) in zip(test_data, test_answer):
     answer = test.prediction(x)
+    if answer != target[0]:
+        wrong += 1
     print("data={}, answer={}, pred={}".format(x, target[0], answer))
+
+print('test percent correct: ', 100 - wronga)
+print('final percent correct: ', 100 - wrong)
